9f3dad52ed44f450fd011f0bf781c3b35b18dca8
Erick Namukolo <erickmndev@gmail.com>
2022-06-08 22:07:02 +0200

    get request 101

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'hello from the server',
-    status: 200,
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
