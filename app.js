9985cfcc4f340b7029a933e17126189a850c6b9f
Erick Namukolo <erickmndev@gmail.com>
2022-06-10 12:30:01 +0200

    delete request 101

--- app.js
+++ app.js
@@ -50,8 +50,32 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   }
   res.status(200).json({
     status: 'success',
-    data: 'updated type here',
+    data: 'updated tour here',
   });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.at(req.params.id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+    return;
+  }
+  const tourIndex = tours.indexOf(tour);
+  tours.splice(tourIndex, 1);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        message: `${tour.name} deleted`,
+      });
+    }
+  );
 });
 
 app.post('/api/v1/tours', (req, res) => {
