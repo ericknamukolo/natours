0ec13cfc9518b3dd4e0c4b6366aef5088ec3dec7
Erick Namukolo <erickmndev@gmail.com>
2022-07-24 13:45:07 +0200

    connect add to remote mongo db database

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const port = process.env.PORT;
+
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
 app.listen(port, () => {
   console.log(`app listening to port ${port}`);
