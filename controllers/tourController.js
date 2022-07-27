8d857aa9a137cc46f725249826d732fa0494c6be
Erick Namukolo <erickmndev@gmail.com>
2022-07-27 21:27:16 +0200

    reading db docs 101

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,24 +1,39 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: newDate,
-    // results: tours.length,
-    data: {
-      //  tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      requestedAt: newDate,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  //  const tour = tours.at(req.params.id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      //   tour,
-    },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
